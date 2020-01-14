f9a4a223dac8d50b0df698335633a88a6984d418
Jenea Vranceanu <jeneavranceanu@gmail.com>
2020-01-14 19:44:25 +0200

    First development commit. Project added as is on this stage.

---
+++ Scripts/MainScene.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_New_Game_Button_pressed():
+	Global.start_new_game()
